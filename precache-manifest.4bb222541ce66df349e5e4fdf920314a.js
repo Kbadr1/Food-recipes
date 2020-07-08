6709f7908a648487dbc1c8b82b900ce788a9f4bb
Kbadr1 <kbadr.dev@gmail.com>
2020-07-09 01:46:11 +0200

    Updates

---
+++ precache-manifest.4bb222541ce66df349e5e4fdf920314a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec1b83c064fe7566e42425134ba36f6",
+    "url": "/react.js-food-recipes/index.html"
+  },
+  {
+    "revision": "df7c539992a36947a488",
+    "url": "/react.js-food-recipes/static/css/main.137090a2.chunk.css"
+  },
+  {
+    "revision": "382258ed9d725a30d0cf",
+    "url": "/react.js-food-recipes/static/js/2.ab2afb7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react.js-food-recipes/static/js/2.ab2afb7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7c539992a36947a488",
+    "url": "/react.js-food-recipes/static/js/main.d88cd939.chunk.js"
+  },
+  {
+    "revision": "cfcb113ff8079b8d118d",
+    "url": "/react.js-food-recipes/static/js/runtime-main.2f76a058.js"
+  }
+]);
